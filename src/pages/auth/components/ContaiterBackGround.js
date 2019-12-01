5eb3728a34fabaef3f913242f30ceee4adc1560e
GENO AUSSADAWUT <35714319+postgeno159@users.noreply.github.com>
2019-12-01 13:10:22 +0700

    update

--- src/pages/auth/components/ContaiterBackGround.js
+++ src/pages/auth/components/ContaiterBackGround.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import PropTypes from 'prop-types';
+    import React from 'react';
+    import CssBaseline from '@material-ui/core/CssBaseline';
+    import Container from '@material-ui/core/Container';
+    import PropTypes from 'prop-types';
 
-function ContaiterBackGround(props) {
+    function ContaiterBackGround(props) {
 
-    return (
-        <CssBaseline>
-            <Container style={{
-                padding: 0
-            }}>
-                <div style={{
-                    backgroundColor: props.backgroundColor !== undefined
-                        ? `${props.backgroundColor}`
-                        : '#fff',
-                    height: props.height !== undefined
-                        ? `${props.height}`
-                        : '100vh'
+        return (
+            <CssBaseline>
+                <Container style={{
+                    padding: 0
                 }}>
-                    {props.children}
-                </div>
-            </Container>
-        </CssBaseline>
-    );
+                    <div style={{
+                        backgroundColor: props.backgroundColor !== undefined
+                            ? `${props.backgroundColor}`
+                            : '#fff',
+                        height: props.height !== undefined
+                            ? `${props.height}`
+                            : '100vh'
+                    }}>
+                        {props.children}
+                    </div>
+                </Container>
+            </CssBaseline>
+        );
 
-};
+    };
 
-ContaiterBackGround.propTypes = {
-    backgroundColor: PropTypes.string,
-    height: PropTypes.string
-};
+    ContaiterBackGround.propTypes = {
+        backgroundColor: PropTypes.string,
+        height: PropTypes.string
+    };
 
-export default ContaiterBackGround;
+    export default ContaiterBackGround;
